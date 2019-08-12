91bf0fd151c35aeffecfb20498509ac46fb31c8d
Pratap A K <pratap.ak@ge.com>
2019-08-12 10:54:07 +0530

    kafka streams processing

--- src/main/java/com/nsc/kafkademo/consumer/MessageReceiver.java
+++ src/main/java/com/nsc/kafkademo/consumer/MessageReceiver.java
@@ -4,10 +4,15 @@ import com.nsc.kafkademo.model.Event;
 import com.nsc.kafkademo.model.PageViewEvent;
 import com.nsc.kafkademo.sink.EventSink;
 import com.nsc.kafkademo.sink.PageViewEventSink;
+import com.nsc.kafkademo.source.PageViewEventSource;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 @EnableBinding({Sink.class, EventSink.class, PageViewEventSink.class})
 public class MessageReceiver {
@@ -25,10 +30,19 @@ public class MessageReceiver {
     }
 
     @StreamListener(target = PageViewEventSink.PAGE_VIEW_INPUT)
-    public void process(KStream<String, PageViewEvent> eventKStream) {
-        eventKStream.foreach((key, value) -> {
-            System.out.println("inside foreach");
-            System.out.println(key + "  " + value);
+    @SendTo(PageViewEventSource.PAGE_COUNT_OUT)
+    public KStream<String, Long> process(KStream<String, PageViewEvent> eventKStream) {
+        return eventKStream.filter((key, value) -> value.getDuration() > 10)
+                .map((key, value) -> new KeyValue<>(value.getPage(), "0"))
+                .groupByKey()
+                .count(Materialized.as("PAGE_COUNT_MV"))
+                .toStream();
+    }
+
+    @StreamListener(target = PageViewEventSink.PAGE_COUNT_IN)
+    public void processKTable(KTable<String, Long> counts) {
+        counts.toStream().foreach((key, value) -> {
+            System.out.println(key + "=" + value);
         });
     }
 }
--- src/main/java/com/nsc/kafkademo/sink/PageViewEventSink.java
+++ src/main/java/com/nsc/kafkademo/sink/PageViewEventSink.java
@@ -2,11 +2,17 @@ package com.nsc.kafkademo.sink;
 
 import com.nsc.kafkademo.model.PageViewEvent;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
 import org.springframework.cloud.stream.annotation.Input;
 
 public interface PageViewEventSink {
     String PAGE_VIEW_INPUT = "pvin";
 
+    String PAGE_COUNT_IN = "pcin";
+
     @Input(PAGE_VIEW_INPUT)
     KStream<String, PageViewEvent> input();
+
+    @Input(PAGE_COUNT_IN)
+    KTable<String, Long> pageCountInput();
 }
